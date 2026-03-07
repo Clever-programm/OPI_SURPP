bc0ac70de8d1e26ef81d7ab23b4d3ba9f511ddd6
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-03-07 16:08:21 +0400

    feat: add employee model

---
+++ backend/app/models/employee.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+
+from app.core.database import Base
+
+class Employee(Base):
+    __tablename__ = "employees"
+    
+    employee_id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    active = Column(Boolean, nullable=False, default=True)
+    
+    # Связи
+    # competences = relationship("EmployeeCompetence", back_populates="employee")
+    # schedule_entries = relationship("Schedule", back_populates="employee")
