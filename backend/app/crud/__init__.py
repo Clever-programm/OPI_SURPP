57557279f889c07bba4d5903f82abe150a1cc21c
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-03-09 22:06:53 +0400

    feat(crud): add equipment crud class

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,7 +1,9 @@
 from .base import CRUDBase
 from .ingredient import crud_ingredient
+from .equipment import crud_equipment
 
 __all__ = [
     "CRUDBase",
     "crud_ingredient",
+    "crud_equipment",
 ]
