bd2678b216874c6031bb1b22081f5b2e2b0d21d2
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-03-09 22:24:02 +0400

    feat(crud): add competence crud class

--- backend/app/crud/__init__.py
+++ backend/app/crud/__init__.py
@@ -1,9 +1,11 @@
 from .base import CRUDBase
 from .ingredient import crud_ingredient
 from .equipment import crud_equipment
+from .competence import crud_competence
 
 __all__ = [
     "CRUDBase",
     "crud_ingredient",
     "crud_equipment",
+    "crud_competence",
 ]
