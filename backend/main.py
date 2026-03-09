3bf81fa69ace3caa2d9b0e669d1b4fd2543b6ea0
i.tsaregorodtsev <tsaregorodtsevigorek@yandex.ru>
2026-03-09 21:50:37 +0400

    feat(routes): add ingredient api router

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from fastapi import FastAPI
 from sqlalchemy import text
 from contextlib import asynccontextmanager
 
+from app.api.v1 import api_router
 from app.core.database import engine
 from app.core.config import settings
 
@@ -82,3 +83,5 @@ app = FastAPI(
     version="1.0.0",
     lifespan=lifespan
     )
+
+app.include_router(api_router, prefix="/api/v1")
